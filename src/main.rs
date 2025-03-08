96b2ed32535ff5f73eee3b5f8cf62389ab689bae
Michal Jurek <jurmcc@gmail.com>
2025-03-08 21:45:24 +0100

    Render System draw sceen (but is not yet ready to be kicked more than once

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use ecs::Entity; // TODO: remove coordinatro from this path
-use ecs::coordinator::Coordinator; // TODO: remove coordinatro from this path
+use ecs::Entity;
+use ecs::coordinator::Coordinator;    // TODO: remove coordinatro from this path
 use ecs::component::ComponentManager; // TODO: remove component from this path
-use ecs::system::System;           // TODO: remove system from this path
+use ecs::system::System;              // TODO: remove system from this path
 
 use raylib::prelude::*;
-use raylib::consts;
-use raylib::core::color;
 
 use std::collections::HashSet;
 use std::any::TypeId;
@@ -21,17 +19,24 @@ pub struct Render {
 
     pub rl: RaylibHandle,
     pub raylib_thread: RaylibThread,
+
+    screen: Screen,
 }
 
 impl Render {
-    pub fn new() -> (Render, Screen) {
+    pub fn new() -> Render {
         let (width, height) = (640, 480);
         let (mut rl, raylib_thread) = raylib::init()
             .size(width, height)
             .title("RenderSystem1")
             .build();
 
-        rl.set_target_fps(60);
+        rl.set_target_fps(5);
+
+        let screen = Screen {
+            width,
+            height,
+        };
 
         let render = Render {
             entities: HashSet::new(),
@@ -39,14 +44,10 @@ impl Render {
 
             rl,
             raylib_thread,
+            screen,
         };
 
-        let screen = Screen {
-            width,
-            height,
-        };
-
-        (render, screen)
+        render
     }
 }
 
@@ -62,10 +63,67 @@ impl System for Render {
         &self.component_types
     }
 
-    fn apply(&self, cm: &mut ComponentManager) {
+    fn apply(&mut self, cm: &mut ComponentManager) {
         println!("Apply for Render");
         for e in self.entities.iter() {
             println!(" e: {}", e);
+        }
+
+        { // TODO: point of focus
+          //       1) move checking "windows_should_close()" out of this system implementation (or
+          //       signal somehot this event to outer world in different way
+          //       2) allow implemenation of this function to just do one 'tick' and then let other
+          //          systems do their work
+            let font_size = 20;
+            let y_min = 12.0;
+            let y_max = self.screen.height as f64 - 12.0 - font_size as f64;
+            let wheel_speed = 4f32;
+
+            let mut ball_pos = (self.screen.width / 2, self.screen.height / 2);
+            let (mut cnt_x, mut cnt_y) = (0.0, 0.0);
+            let mut wheel_v = Vector2 { x: (self.screen.width / 4) as f32, y: (self.screen.height / 4) as f32 };
+
+            while !self.rl.window_should_close() {
+
+                let wheel_move_v = self.rl.get_mouse_wheel_move_v();
+                (wheel_v.x, wheel_v.y) = (wheel_v.x + wheel_move_v.x, wheel_v.y + wheel_move_v.y);
+
+                if self.rl.is_key_down(KeyboardKey::KEY_RIGHT) {
+                    if !(ball_pos.0 >= (self.screen.width)) {
+                        ball_pos.0 += 2;
+                    }
+                }
+                if self.rl.is_key_down(KeyboardKey::KEY_LEFT) {
+                    if !(ball_pos.0 <= 0) {
+                        ball_pos.0 -= 2;
+                    }
+                }
+                if self.rl.is_key_down(KeyboardKey::KEY_UP) {
+                    if !(ball_pos.1 <= 0) {
+                        ball_pos.1 -= 2;
+                    }
+                }
+                if self.rl.is_key_down(KeyboardKey::KEY_DOWN) {
+                    if !(ball_pos.1 >= self.screen.height) {
+                        ball_pos.1 += 2;
+                    }
+                }
+
+                let x = (self.screen.width - 50) as f64 / 2f64 + ((self.screen.width as f64 / 2f64)-30f64)*(((3.14*cnt_x/360f64) as f64).sin());
+                let y = (y_min + (y_max - y_min)*((2f64*3.14f64 * cnt_y / 360.0).cos())).abs();
+                let mouse_pos = self.rl.get_mouse_position();
+                let mut d = self.rl.begin_drawing(&self.raylib_thread);
+
+                d.draw_rectangle_v(wheel_v, Vector2 { x: wheel_v.x + wheel_speed * 10f32, y: wheel_v.y + wheel_speed * 10f32}, Color::YELLOWGREEN);
+                d.draw_circle(ball_pos.0, ball_pos.1, 10f32, Color::MAROON);
+                d.clear_background(Color::WHITE);
+                d.draw_text("Hello", x.round() as i32, y.round() as i32, font_size, Color::BLACK);
+
+                d.draw_circle_v(mouse_pos, 20f32, Color::BLUE);
+
+                cnt_x += 2.0 * 1.7;
+                cnt_y += 4.0 * 1.0;
+            }
         }
     }
 }
@@ -79,68 +137,8 @@ fn main() {
     c.add_component(e1, 1i32);
     c.add_component(e1, 2f32);
 
-    let (mut render_sys, screen) = Render::new();
-    {
-        c.kick_all_systems(); // TODO: point of focuse
-                              // add some system (other than render)
-                              // and see if it works
-
-        let font_size = 20;
-        let y_min = 12.0;
-        let y_max = screen.height as f64 - 12.0 - font_size as f64;
-        let wheel_speed = 4f32;
-
-        let mut ball_pos = (screen.width / 2, screen.height / 2);
-        let (mut cnt_x, mut cnt_y) = (0.0, 0.0);
-        let mut wheel_v = Vector2 { x: (screen.width / 4) as f32, y: (screen.height / 4) as f32 };
-
-        let rl = &mut render_sys.rl;
-        let thread = &render_sys.raylib_thread;
-        while !rl.window_should_close() {
-
-            let wheel_move_v = rl.get_mouse_wheel_move_v();
-            (wheel_v.x, wheel_v.y) = (wheel_v.x + wheel_move_v.x, wheel_v.y + wheel_move_v.y);
-
-            if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
-                if !(ball_pos.0 >= (screen.width)) {
-                    ball_pos.0 += 2;
-                }
-            }
-            if rl.is_key_down(KeyboardKey::KEY_LEFT) {
-                if !(ball_pos.0 <= 0) {
-                    ball_pos.0 -= 2;
-                }
-            }
-            if rl.is_key_down(KeyboardKey::KEY_UP) {
-                if !(ball_pos.1 <= 0) {
-                    ball_pos.1 -= 2;
-                }
-            }
-            if rl.is_key_down(KeyboardKey::KEY_DOWN) {
-                if !(ball_pos.1 >= screen.height) {
-                    ball_pos.1 += 2;
-                }
-            }
-
-            let x = (screen.width - 50) as f64 / 2f64 + ((screen.width as f64 / 2f64)-30f64)*(((3.14*cnt_x/360f64) as f64).sin());
-            let y = (y_min + (y_max - y_min)*((2f64*3.14f64 * cnt_y / 360.0).cos())).abs();
-            let mouse_pos = rl.get_mouse_position();
-            let mut d = rl.begin_drawing(&thread);
-
-            d.draw_rectangle_v(wheel_v, Vector2 { x: wheel_v.x + wheel_speed * 10f32, y: wheel_v.y + wheel_speed * 10f32}, Color::YELLOWGREEN);
-            d.draw_circle(ball_pos.0, ball_pos.1, 10f32, Color::MAROON);
-            d.clear_background(Color::WHITE);
-            d.draw_text("Hello", x.round() as i32, y.round() as i32, font_size, Color::BLACK);
-
-            d.draw_circle_v(mouse_pos, 20f32, Color::BLUE);
-
-            cnt_x += 2.0 * 1.7;
-            cnt_y += 4.0 * 1.0;
-        }
-    }
-
+    let render_sys = Render::new();
     c.register_system(render_sys);
+
     c.kick_all_systems();
-
-
 }
